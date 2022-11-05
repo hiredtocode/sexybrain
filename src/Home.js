10903ed4f24038cce5e0ae193278e1a10823c560
hiredtocode <jasonhan@kakao.com>
2022-11-05 19:39:58 +0900

    react-router-dom bug fixed

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+// import BlogList from "./BlogList";
+// import useFetch from "./useFetch";
+
+const Home = () => {
+	// const {
+	// 	data: blogs,
+	// 	isLoading,
+	// 	error,
+	// } = useFetch("http://localhost:8000/blogs");
+
+	return (
+		<div className="home">
+			{/* {error && <div>{error}</div>}
+			{isLoading && <div>Loading...</div>}
+			{blogs && (
+				<BlogList blogs={blogs} title="Awesome Blog!" />
+			)} */}
+		</div>
+	);
+};
+
+export default Home;
