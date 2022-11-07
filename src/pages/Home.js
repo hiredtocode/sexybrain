950118a708e32b65ba03e8af156bc503578cd4fa
hiredtocode <jasonhan@kakao.com>
2022-11-07 20:33:08 +0900

    Blog list displays on home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,34 @@
-// import BlogList from "./BlogList";
+import BlogList from "./BlogList";
+import db from "../firebase";
+import { useState, useEffect } from "react";
+import { collection, onSnapshot } from "firebase/firestore";
 // import useFetch from "./useFetch";
 
-const Home = () => {
-	// const {
-	// 	data: blogs,
-	// 	isLoading,
-	// 	error,
-	// } = useFetch("http://localhost:8000/blogs");
+function Home() {
+	const [blogs, setBlogs] = useState([]);
 
+	const blogCollectionRef = collection(db, "blogPosts");
+
+	useEffect(() => {
+		onSnapshot(blogCollectionRef, (snapshot) => {
+			setBlogs(
+				snapshot.docs.map((doc) => {
+					return {
+						id: doc.id,
+						viewing: false,
+						...doc.data(),
+					};
+				})
+			);
+		});
+	}, []);
 	return (
 		<div className="home">
-			{/* {error && <div>{error}</div>}
-			{isLoading && <div>Loading...</div>}
-			{blogs && (
-				<BlogList blogs={blogs} title="Awesome Blog!" />
-			)} */}
+			{/* {error && <div>{error}</div>} */}
+			{/* {isLoading && <div>Loading...</div>} */}
+			{blogs && <BlogList blogs={blogs} title="Awesome Blog!" />}
 		</div>
 	);
-};
+}
 
 export default Home;
