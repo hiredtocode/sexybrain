2345bbbfc363f7dd07bee40ec80cf2a8bf8f6f24
hiredtocode <jasonhan@kakao.com>
2022-11-07 12:21:42 +0900

    removed unused code

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -30,31 +30,17 @@ const Create = () => {
 			<h2>Add a New Blog</h2>
 			<form onSubmit={handleSubmit}>
 				<label>Blog title:</label>
-				<input
-					type="text"
-					value={title}
-					onChange={(e) => setTitle(e.target.value)}
-					required
-				/>
+				<input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
 				<label>Blog body:</label>
-				<textarea
-					value={body}
-					onChange={(e) => setBody(e.target.value)}
-					required
-				></textarea>
+				<textarea value={body} onChange={(e) => setBody(e.target.value)} required></textarea>
 				<label>Blog author:</label>
-				<select
-					value={author}
-					onChange={(e) => setAuthor(e.target.value)}
-				>
+				<select value={author} onChange={(e) => setAuthor(e.target.value)}>
 					<option value="mario">mario</option>
 					<option value="yoshi">yoshi</option>
 				</select>
 
 				{!isLoading && <button>Add Blog</button>}
-				{isLoading && (
-					<button disabled>Adding Blog...</button>
-				)}
+				{isLoading && <button disabled>Adding Blog...</button>}
 			</form>
 		</div>
 	);
