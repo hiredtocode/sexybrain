b0954b41fe2cce6d1f55eb399fed362c45bb7933
hiredtocode <jasonhan@kakao.com>
2022-11-09 19:48:31 +0900

    Toast, auth, routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Create from './pages/Create';
+import AddEdit from './pages/AddEdit';
 import BlogDetails from './pages/BlogDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import Resume from './pages/Resume';
+import Auth from './pages/Auth';
 import './App.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { auth } from './firebase.config';
 
 function App() {
+	const [active, setActive] = useState('home');
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				setUser(authUser);
+			} else {
+				setUser(null);
+			}
+		});
+	}, []);
+
 	return (
 		<Router>
 			<div className='App'>
-				<Navbar />
+				<Navbar setActive={setActive} active={active} user={user} />
 				<div className='content'>
+					<ToastContainer />
 					<Routes>
 						<Route path='/' element={<Home />} />
-						<Route path='/create' element={<Create />} />
 						<Route path='/resume' element={<Resume />} />
+						<Route path='/create' element={<AddEdit />} />
+						<Route path='/update/:id' element={<AddEdit />} />
 						<Route path='/blogs/:id' element={<BlogDetails />} />
+						<Route path='/auth' element={<Auth />} />
 						<Route path='*' element={<NotFound />} />
 					</Routes>
 				</div>
