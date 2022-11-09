6367380b6aac9b736be9c8cfc1e21ef8f9ef80e5
hiredtocode <jasonhan@kakao.com>
2022-11-09 19:50:06 +0900

    Logged in and out state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ active, setActive, user, handleLogout }) => {
+	const userId = user?.uid;
+	console.log('userId:', userId);
+	console.log('name:', user?.displayName);
+
 	return (
-		<nav className="navbar">
+		<nav className='navbar'>
 			<h1>The Blog</h1>
-			<div className="links">
-				<Link to="/">Home</Link>
-				<Link to="/create">New Blog</Link>
+			<div className='links'>
+				<Link to='/'>
+					<li
+						className={`navbar ${active === 'home' ? 'active' : ''}`}
+						onClick={() => setActive('home')}
+					>
+						Home
+					</li>
+				</Link>
+				<Link to='/create'>
+					<li
+						onClick={() => setActive('create')}
+						className={`navbar ${active === 'create' ? 'active' : ''}`}
+					>
+						Create
+					</li>
+				</Link>
 			</div>
+			{userId ? (
+				<>
+					<div className='profile-logo'>
+						<img
+							src='https://cdn-icons-png.flaticon.com/512/149/149071.png'
+							alt='logo'
+							style={{
+								width: '30px',
+								height: '30px',
+								borderRadius: '50%',
+								marginTop: '12px',
+							}}
+						/>
+					</div>
+					<p style={{ marginTop: '12px', marginLeft: '5px' }}>{user?.displayName}</p>
+					<li className='nav-item nav-link' onClick={handleLogout}>
+						Logout
+					</li>
+				</>
+			) : (
+				<Link to='/auth' style={{ textDecoration: 'none' }}>
+					<li
+						className={`nav-item nav-link ${active === 'login' ? 'active' : ''}`}
+						onClick={() => setActive('login')}
+					>
+						Login
+					</li>
+				</Link>
+			)}
 		</nav>
 	);
 };
