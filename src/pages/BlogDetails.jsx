b276467da9151b1400a0ea11247d25dd048c390c
hiredtocode <jasonhan@kakao.com>
2022-11-10 22:59:57 +0900

    blog detail page works

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,39 +1,67 @@
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { doc, getDoc } from 'firebase/firestore';
-import { useState, useEffect } from 'react';
+import Tags from '../components/Tags';
 import { db } from '../firebase.config';
 
-const BlogDetails = () => {
+const Detail = ({ setActive }) => {
 	const { id } = useParams();
 	const [blog, setBlog] = useState(null);
-	console.log('blog:', blog);
+	const [blogs, setBlogs] = useState([]);
+	const [tags, setTags] = useState([]);
+
+	useEffect(() => {
+		const getBlogsData = async () => {
+			const blogRef = collection(db, 'blogPosts');
+			const blogs = await getDocs(blogRef);
+			setBlogs(blogs.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+			let tags = [];
+			blogs.docs.map((doc) => tags.push(...doc.get('tags')));
+			let uniqueTags = [...new Set(tags)];
+			setTags(uniqueTags);
+		};
+
+		getBlogsData();
+	}, []);
 
 	useEffect(() => {
 		id && getBlogDetail();
-		// eslint-disable-next-line
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [id]);
 
 	const getBlogDetail = async () => {
 		const docRef = doc(db, 'blogPosts', id);
 		const blogDetail = await getDoc(docRef);
 		setBlog(blogDetail.data());
+		setActive(null);
 	};
-
-	const removePost = async () => {};
-
 	return (
-		<div className='blog-details'>
-			{/* {isLoading && <div>Loading...</div>}
-			{error && <div>{error}</div>} */}
-			{blog && (
-				<article>
-					<h2>{blog.title}</h2>
-					<p>Written by {blog.author}</p>
-					<div>{blog.body}</div>
-					<button onClick={removePost}>Delete</button>
-				</article>
-			)}
+		<div className='single'>
+			<div className='blog-title-box' style={{ backgroundImage: `url('${blog?.imgUrl}')` }}>
+				<div className='overlay'></div>
+				<div className='blog-title'>
+					<span>{blog?.timestamp.toDate().toDateString()}</span>
+					<h2>{blog?.title}</h2>
+				</div>
+			</div>
+			<div className='container-fluid pb-4 pt-4 padding blog-single-content'>
+				<div className='container padding'>
+					<div className='row mx-0'>
+						<div className='col-md-8'>
+							<span className='meta-info text-start'>
+								By <p className='author'>{blog?.author}</p> -&nbsp;
+								{blog?.timestamp.toDate().toDateString()}
+							</span>
+							<p className='text-start'>{blog?.body}</p>
+						</div>
+						<div className='col-md-3'>
+							<Tags tags={tags} />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
-export default BlogDetails;
+
+export default Detail;
