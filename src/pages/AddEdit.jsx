2ee427002e0762a529914b994a68ff30c840e4cc
hiredtocode <jasonhan@kakao.com>
2022-11-10 14:28:09 +0900

    Posting blog works but still have to work on tags

--- src/pages/AddEdit.jsx
+++ src/pages/AddEdit.jsx
@@ -1,34 +1,14 @@
-import { useState, useRef } from 'react';
-import { addDoc, collection } from 'firebase/firestore';
-import { db } from '../firebase.config';
+import { useState, useEffect } from 'react';
+import { addDoc, collection, getDoc, serverTimestamp, doc, updateDoc } from 'firebase/firestore';
+import { db, storage } from '../firebase.config';
 import { TagInput } from '../components/Tags';
+import ReactTagInput from '@pathofdev/react-tag-input';
+import '@pathofdev/react-tag-input/build/index.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { toast } from 'react-toastify';
 
-const Create = () => {
-	const recipesCollectionRef = collection(db, 'blogPosts');
-	const [isLoading, setIsLoading] = useState(false);
-	const [form, setForm] = useState({
-		title: '',
-		body: '',
-		category: '',
-		tags: [],
-	});
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (!form.title || !form.body || !form.category) {
-			alert('Please fill out all fields');
-			return;
-		}
-
-		addDoc(recipesCollectionRef, form);
-
-		setForm({
-			title: '',
-			body: '',
-			category: '',
-		});
-	};
-
+const AddEdit = ({ user, setActive }) => {
 	const categoryOption = [
 		'JavaScript',
 		'TypeScript',
@@ -41,7 +21,101 @@ const Create = () => {
 		'Material UI',
 		'Tailwind CSS',
 		'Styled Components',
+		'What I learned',
 	];
+	const [form, setForm] = useState({
+		title: '',
+		tags: [],
+		category: '',
+		body: '',
+	});
+	const [file, setFile] = useState(null);
+	const [progress, setProgress] = useState(null);
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const blogCollectionRef = collection(db, 'blogPosts');
+	const [isLoading, setIsLoading] = useState(false);
+
+	const { title, body, tags, category } = form;
+
+	useEffect(() => {
+		id && getBlogDetail();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [id]);
+
+	const getBlogDetail = async () => {
+		const docRef = doc(db, 'blogPosts', id);
+		const snapshot = await getDoc(docRef);
+		if (snapshot.exists()) {
+			setForm({ ...snapshot.data() });
+		}
+		setActive(null);
+	};
+
+	// Submit handle
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		console.log('e from handleSubmit:', e);
+		uploadFile(file);
+		if (category && title && body) {
+			if (!id) {
+				try {
+					await addDoc(blogCollectionRef, {
+						...form,
+						timestamp: serverTimestamp(),
+						author: user.displayName,
+						userId: user.uid,
+					});
+					toast.success('Blog created successfully');
+					console.log('user.uid:', user.uid);
+				} catch (err) {
+					console.log(err);
+				}
+			} else {
+				try {
+					await updateDoc(doc(db, 'blogPosts', id), {
+						...form,
+						timestamp: serverTimestamp(),
+						author: user.displayName,
+						userId: user.uid,
+					});
+					toast.success('Blog updated successfully');
+				} catch (err) {
+					console.log(err);
+				}
+			}
+		} else {
+			return toast.error('All fields are mandatory to fill');
+		}
+		navigate('/');
+	};
+	// Submit handle end
+
+	// Upload file
+	const uploadFile = (file) => {
+		if (!file) return;
+
+		const storageRef = ref(storage, file.name);
+		const uploadTask = uploadBytesResumable(storageRef, file);
+
+		uploadTask.on(
+			'state_changed',
+			(snapshot) => {
+				const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+
+				setProgress(progress);
+			},
+			(error) => console.log('error:', error),
+			() => {
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+					toast.info(`Image upload to firebase successfully and the URL is: ${downloadUrl}`);
+					setForm((prev) => ({ ...prev, imgUrl: downloadUrl }));
+				});
+			}
+		);
+	};
+	// Upload file end
 
 	const onCategoryChange = (e) => {
 		setForm({ ...form, category: e.target.value });
@@ -53,31 +127,85 @@ const Create = () => {
 		setForm({ ...form, body: e.target.value });
 	};
 
+	const handleTags = (tags) => {
+		setForm({ ...form, tags });
+	};
 	return (
-		<div className='create'>
-			<h2>Add a New Blog</h2>
-			<form onSubmit={handleSubmit}>
-				<label>Blog title:</label>
-				<input type='text' value={form.title} onChange={onTitleChange} required />
-				<label>Blog body:</label>
-				<textarea value={form.body} onChange={onBodyChange} required />
-				<div className='category'>
-					<select value={form.category} onChange={onCategoryChange} className='catg-dropdown'>
-						<option>Please select category</option>
-						{categoryOption.map((selection, index) => (
-							<option value={selection || ''} key={index}>
-								{selection}
-							</option>
-						))}
-					</select>
+		<div className='container-fluid mb-4'>
+			<div className='container'>
+				<div className='col-12'>
+					<div className='text-center heading py-2'>{id ? 'Update Blog' : 'Create Blog'}</div>
 				</div>
-				<label>Tag:</label>
+				<div className='row h-100 justify-content-center align-items-center'>
+					<div className='col-10 col-md-8 col-lg-6'>
+						<form className='row blog-form' onSubmit={handleSubmit}>
+							{/* Title section  */}
+							<div className='col-12 py-3'>
+								<input
+									type='text'
+									className='form-control input-text-box'
+									placeholder='Title'
+									name='title'
+									value={title}
+									onChange={onTitleChange}
+								/>
+							</div>
+							{/* Title section end */}
 
-				{!isLoading && <button type='submit'>Add Blog</button>}
-				{isLoading && <button disabled>Adding Blog...</button>}
-			</form>
+							{/* Tag section */}
+							{/* <div className='col-12 py-3'>
+								<ReactTagInput tags={tags} placeholder='Tags' onChange={handleTags} />
+							</div> */}
+							{/* Tag section end */}
+
+							{/* category section */}
+							<div className='col-12 py-3'>
+								<select value={category} onChange={onCategoryChange} className='catg-dropdown'>
+									<option>Please select category</option>
+									{categoryOption.map((option, index) => (
+										<option value={option || ''} key={index}>
+											{option}
+										</option>
+									))}
+								</select>
+							</div>
+							{/* category section end */}
+							{/* body section */}
+							<div className='col-12 py-3'>
+								<textarea
+									className='form-control body-box'
+									placeholder='body'
+									value={body}
+									name='body'
+									onChange={onBodyChange}
+								/>
+							</div>
+							{/* body section end  */}
+							{/* file upload section */}
+							<div className='mb-3'>
+								<input
+									type='file'
+									className='form-control'
+									onChange={(e) => setFile(e.target.files[0])}
+								/>
+							</div>
+							<div className='col-12 py-3 text-center'>
+								<h3>Uploaded {progress}%</h3>
+								<button
+									className='btn btn-add'
+									type='submit'
+									disabled={progress !== null && progress < 100}
+								>
+									{id ? 'Update' : 'Submit'}
+								</button>
+							</div>
+							{/* file upload section end  */}
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default Create;
+export default AddEdit;
