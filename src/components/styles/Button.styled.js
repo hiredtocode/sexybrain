a2d12795213bc32863f9306b409de758d53689d3
hiredtocode <jasonhan@kakao.com>
2022-11-12 02:06:04 +0900

    hover effect added to button

--- src/components/styles/Button.styled.js
+++ src/components/styles/Button.styled.js
@@ -1,18 +1,28 @@
 import styled from 'styled-components';
 
-export const Button = styled.button`
+const Button = styled.button`
 	border-radius: 50px;
 	border: none;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
 	cursor: pointer;
 	font-size: 16px;
 	font-weight: 700;
-	padding: 15px 60px;
+	margin: 0 5px;
+	padding: 15px 40px;
 	background-color: ${({ bg }) => bg || '#fff'};
 	color: ${({ color }) => color || '#333'};
 
 	&:hover {
 		opacity: 0.9;
+		transition: 0.8s;
 		transform: scale(0.98);
+		box-shadow: 0 5px 20px rgba(0, 0, 0, 0.3);
+		background: linear-gradient(45deg, #3776d3, #55c1ff);
+	}
+
+	&:hover span {
+		color: #fff;
 	}
 `;
+
+export default Button;
