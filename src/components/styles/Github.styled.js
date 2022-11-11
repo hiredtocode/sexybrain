0c5888122f4bd5760096461a97399155d81d7d70
hiredtocode <jasonhan@kakao.com>
2022-11-12 02:07:30 +0900

    github logo styled

---
+++ src/components/styles/Github.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const GithubImageBlack = styled.img`
+	width: 55px;
+	height: 55px;
+
+	&:hover {
+		opacity: 1;
+	}
+`;
+
+export const GithubImageWhite = styled.img`
+	position: absolute;
+	transition: 0.6s;
+	opacity: 0;
+	width: 55px;
+	height: 55px;
+	background: white;
+
+	&:hover {
+		opacity: 1;
+	}
+`;
+
+export const GithubImageContainer = styled.a`
+	position: relative;
+	/* width: 70px;
+	height: 70px; */
+	padding-left: 20px;
+	align-items: center;
+`;
