6395470999a4dde1d92109ba33649a65be0faf2d
hiredtocode <jasonhan@kakao.com>
2022-11-12 02:06:25 +0900

    resume container added

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -1,9 +1,40 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const ResumeContainer = styled.div`
 	display: flex;
-	width: 1200px;
+	max-width: 1200px;
 	justify-content: space-between;
 	align-items: center;
 	transition: 0.6s;
+	padding: 150px 50px;
+	margin: 0 auto;
+
+	@media (max-width: ${({ theme }) => theme.xs}) {
+	}
+	@media (max-width: ${({ theme }) => theme.s}) {
+	}
+	@media (max-width: ${({ theme }) => theme.md}) {
+	}
+	@media (max-width: ${({ theme }) => theme.lg}) {
+		padding: 100px 30px;
+	} ;
+`;
+export const Container = styled.div`
+	display: flex;
+	max-width: 1200px;
+	justify-content: space-between;
+	align-items: center;
+	transition: 0.6s;
+	padding: 100px 50px;
+	margin: 0 auto;
+
+	@media (max-width: ${({ theme }) => theme.xs}) {
+	}
+	@media (max-width: ${({ theme }) => theme.s}) {
+	}
+	@media (max-width: ${({ theme }) => theme.md}) {
+	}
+	@media (max-width: ${({ theme }) => theme.lg}) {
+		padding: 100px 30px;
+	} ;
 `;
