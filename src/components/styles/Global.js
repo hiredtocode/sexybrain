2ef4391165123d108ceb77f91711ba021b8b7604
hiredtocode <jasonhan@kakao.com>
2022-11-12 02:08:21 +0900

    Global font size up

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -15,30 +15,13 @@ body{
 }
 
 p{
-  opacity: 0.6;
-  line-height: 1.5;
-
+	font-size: 1.2em;
 }
 
 img {
   max-width: 100%;
 }
 
-/* Small devices (landscape phones, 576px and up) */
-@media (min-width: 576px) {
-}
-
-/* Medium devices (tablets, 768px and up) */
-@media (min-width: 768px) {
-}
-
-/* Large devices (desktops, 992px and up) */
-@media (min-width: 992px) {
-}
-
-/* Extra large devices (large desktops, 1200px and up) */
-@media (min-width: 1200px) {
-}
 
 `;
 export default GlobalStyles;
