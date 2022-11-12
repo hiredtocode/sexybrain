7674527335745bf81ea1d9a295534756526de98a
hiredtocode <jasonhan@kakao.com>
2022-11-12 17:00:22 +0900

    minor change

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -1,31 +1,12 @@
 import styled from 'styled-components';
 
-export const ResumeContainer = styled.div`
-	display: flex;
-	max-width: 1200px;
-	justify-content: space-between;
-	align-items: center;
-	transition: 0.6s;
-	padding: 150px 50px;
-	margin: 0 auto;
-
-	@media (max-width: ${({ theme }) => theme.xs}) {
-	}
-	@media (max-width: ${({ theme }) => theme.s}) {
-	}
-	@media (max-width: ${({ theme }) => theme.md}) {
-	}
-	@media (max-width: ${({ theme }) => theme.lg}) {
-		padding: 100px 30px;
-	} ;
-`;
 export const Container = styled.div`
 	display: flex;
 	max-width: 1200px;
 	justify-content: space-between;
 	align-items: center;
+	padding: 130px 30px;
 	transition: 0.6s;
-	padding: 100px 50px;
 	margin: 0 auto;
 
 	@media (max-width: ${({ theme }) => theme.xs}) {
