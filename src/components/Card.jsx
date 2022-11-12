025e09aba87d2b79f27afbde8916186005570933
hiredtocode <jasonhan@kakao.com>
2022-11-12 16:59:36 +0900

    Cards display correctly now

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,47 +4,51 @@ import { excerpt } from '../utility';
 import Button from './styles/Button.styled.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { Container } from './styles/Container.styled.js';
 
 export default function Card({ blogs, user, handleDelete }) {
 	const userId = user?.uid;
 
 	return (
-		<StyledCard>
+		<>
 			{blogs?.map((item, index) => (
-				<div key={index}>
-					<img src={item.imgUrl} alt={item.title} />
+				<StyledCard key={index}>
+					<div>
+						<img src={item.imgUrl} alt={item.title} />
+					</div>
+					<div>
+						<h4>{item.category}</h4>
+						<h2>{item.title}</h2>
 
-					<h6>{item.category}</h6>
-					<span>{item.title}</span>
-
-					<span>
-						<p>{item.author}</p> -&nbsp;
-						{item.timestamp.toDate().toDateString()}
-					</span>
-					<div>{excerpt(item.body, 120)}</div>
-					<Link to={`/detail/${item.id}`}>
-						<Button>Read More</Button>
-					</Link>
-					{userId && item.userId === userId && (
-						<div style={{ float: 'right' }}>
-							<FontAwesomeIcon
-								icon={faTrash}
-								style={{ cursor: 'pointer' }}
-								size='2x'
-								onClick={() => handleDelete(item.id)}
-							/>
-							<Link to={`/update/${item.id}`}>
+						<span>
+							<p>By: {item.author}</p> -&nbsp;
+							{item.timestamp.toDate().toDateString()}
+						</span>
+						<div>{excerpt(item.body, 120)}</div>
+						<Link to={`/detail/${item.id}`}>
+							<Button>Read More</Button>
+						</Link>
+						{userId && item.userId === userId && (
+							<div style={{ float: 'right' }}>
 								<FontAwesomeIcon
-									icon={faEdit}
-									name='edit'
+									icon={faTrash}
 									style={{ cursor: 'pointer' }}
 									size='2x'
+									onClick={() => handleDelete(item.id)}
 								/>
-							</Link>
-						</div>
-					)}
-				</div>
+								<Link to={`/update/${item.id}`}>
+									<FontAwesomeIcon
+										icon={faEdit}
+										name='edit'
+										style={{ cursor: 'pointer' }}
+										size='2x'
+									/>
+								</Link>
+							</div>
+						)}
+					</div>
+				</StyledCard>
 			))}
-		</StyledCard>
+		</>
 	);
 }
