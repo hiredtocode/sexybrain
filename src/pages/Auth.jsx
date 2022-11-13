15316fe0c700cf19048b8a0c9bdcad15c17de994
hiredtocode <jasonhan@kakao.com>
2022-11-13 22:53:41 +0900

    Removed unused code and added styled components

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -7,20 +7,25 @@ import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { auth } from '../firebase.config';
 import { useNavigate } from 'react-router-dom';
+import {
+	FormContainer,
+	FormInput,
+	FormLabel,
+	Form,
+	FormButton,
+} from '../components/styles/FormContainer.styled.js';
+import './Auth.scss';
 
 const initialState = {
-	firstName: '',
-	lastName: '',
 	email: '',
 	password: '',
-	confirmPassword: '',
 };
 
 const Auth = ({ setActive, setUser }) => {
 	const [state, setState] = useState(initialState);
 	const [signUp, setSignUp] = useState(false);
 
-	const { email, password, firstName, lastName, confirmPassword } = state;
+	const { email, password } = state;
 
 	const navigate = useNavigate();
 
@@ -38,133 +43,53 @@ const Auth = ({ setActive, setUser }) => {
 			} else {
 				return toast.error('All fields are mandatory to fill');
 			}
-		} else {
-			if (password !== confirmPassword) {
-				return toast.error("Password don't match");
-			}
-			if (firstName && lastName && email && password) {
-				const { user } = await createUserWithEmailAndPassword(auth, email, password);
-				await updateProfile(user, { displayName: `${firstName} ${lastName}` });
-				setActive('home');
-			} else {
-				return toast.error('All fields are mandatory to fill');
-			}
 		}
+
 		navigate('/');
 	};
 
 	return (
-		<div className=''>
-			<div className=''>
-				<div className=''>
-					<div className=''>{!signUp ? 'Sign-In' : 'Sign-Up'}</div>
-				</div>
-				<div className=''>
-					<div className=''>
-						<form className='' onSubmit={handleAuth}>
-							{/* {signUp && (
-								<>
-									<div className='col-6 py-3'>
-										<input
-											type='text'
-											className='form-control input-text-box'
-											placeholder='First Name'
-											name='firstName'
-											value={firstName}
-											onChange={handleChange}
-										/>
-									</div>
-									<div className='col-6 py-3'>
-										<input
-											type='text'
-											className='form-control input-text-box'
-											placeholder='Last Name'
-											name='lastName'
-											value={lastName}
-											onChange={handleChange}
-										/>
-									</div>
-								</>
-							)} */}
-							<div className=''>
-								<input
-									type='email'
-									className='form-control input-text-box'
-									placeholder='Email'
-									name='email'
-									value={email}
-									onChange={handleChange}
-								/>
-							</div>
-							<div className=''>
-								<input
-									type='password'
-									className='form-control input-text-box'
-									placeholder='Password'
-									name='password'
-									value={password}
-									onChange={handleChange}
-								/>
-							</div>
-							{signUp && (
-								<div className=''>
-									<input
-										type='password'
-										className=''
-										placeholder='Confirm Password'
-										name='confirmPassword'
-										value={confirmPassword}
-										onChange={handleChange}
-									/>
-								</div>
-							)}
+		<>
+			<FormContainer>
+				<div className='l-form'>
+					<Form className='form' onSubmit={handleAuth}>
+						<h1 className='form__title'>{!signUp ? 'Sign-In' : 'Sign-Up'}</h1>
 
-							<div className=''>
-								<button className={`btn ${!signUp ? 'btn-sign-in' : 'btn-sign-up'}`} type='submit'>
-									{!signUp ? 'Sign-in' : 'Sign-up'}
-								</button>
-							</div>
-						</form>
-						{/* <div>
-							{!signUp ? (
-								<>
-									<div className='text-center justify-content-center mt-2 pt-2'>
-										<p className='small fw-bold mt-2 pt-1 mb-0'>
-											Don't have an account ?&nbsp;
-											<span
-												className='link-danger'
-												style={{ textDecoration: 'none', cursor: 'pointer' }}
-												onClick={() => setSignUp(true)}
-											>
-												Sign Up
-											</span>
-										</p>
-									</div>
-								</>
-							) : (
-								<>
-									<div className='text-center justify-content-center mt-2 pt-2'>
-										<p className='small fw-bold mt-2 pt-1 mb-0'>
-											Already have an account ?&nbsp;
-											<span
-												style={{
-													textDecoration: 'none',
-													cursor: 'pointer',
-													color: '#298af2',
-												}}
-												onClick={() => setSignUp(false)}
-											>
-												Sign In
-											</span>
-										</p>
-									</div>
-								</>
-							)}
-						</div> */}
-					</div>
+						<div className='form__div'>
+							<FormInput
+								type='email'
+								autoComplete='username'
+								className='form__input'
+								name='email'
+								value={email}
+								onChange={handleChange}
+							/>
+							<FormLabel htmlFor='email' className=''>
+								Email
+							</FormLabel>
+						</div>
+
+						<div className='form__div'>
+							<FormInput
+								type='password'
+								autoComplete='current-password'
+								className='form__input'
+								name='password'
+								value={password}
+								onChange={handleChange}
+							/>
+							<FormLabel htmlFor='password' className=''>
+								Password
+							</FormLabel>
+						</div>
+
+						<FormButton className={`btn ${!signUp ? 'btn-sign-in' : 'btn-sign-up'}`} type='submit'>
+							{!signUp ? 'Login' : 'Sign-up'}
+						</FormButton>
+					</Form>
 				</div>
-			</div>
-		</div>
+			</FormContainer>
+		</>
 	);
 };
 
