7ca3a05d2014528a53afbcad9e59db4ac9a062c7
hiredtocode <jasonhan@kakao.com>
2022-11-13 22:52:06 +0900

    takes in more arguments

--- src/components/styles/Flex.styled.js
+++ src/components/styles/Flex.styled.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const Flex = styled.div`
 	display: flex;
 	flex-direction: ${({ direction }) => direction};
+	justify-content: ${({ justify }) => justify};
+	align-items: ${({ align }) => align};
+	margin: 0 10px;
 `;
 
 export default Flex;
