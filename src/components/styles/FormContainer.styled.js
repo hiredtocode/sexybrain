1efe53ef14fa1fc8dc2df33eab77935719db827f
hiredtocode <jasonhan@kakao.com>
2022-11-15 10:52:10 +0900

    Cancel button added

--- src/components/styles/FormContainer.styled.js
+++ src/components/styles/FormContainer.styled.js
@@ -31,7 +31,7 @@ export const FormButton = styled.button`
 	cursor: pointer;
 	font-size: 1.2rem;
 	font-weight: 700;
-	margin: 0 auto;
+	margin: 0 10px;
 	width: 100%;
 	padding: 10px 15px;
 	background-color: ${({ bg }) => bg || '#fff'};
@@ -43,6 +43,32 @@ export const FormButton = styled.button`
 		transform: scale(0.98);
 		box-shadow: 0 5px 20px rgba(0, 0, 0, 0.3);
 		background: linear-gradient(45deg, #3776d3, #55c1ff);
+		color: white;
+	}
+
+	&:hover span {
+		color: #fff;
+	}
+`;
+export const FormCancelButton = styled.button`
+	border-radius: 0.5rem;
+	border: none;
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+	cursor: pointer;
+	font-size: 1.2rem;
+	font-weight: 700;
+	margin: 0 10px;
+	width: 100%;
+	padding: 10px 15px;
+	background-color: ${({ bg }) => bg || '#fff'};
+	color: ${({ color }) => color || '#333'};
+
+	&:hover {
+		opacity: 0.9;
+		transition: 0.5s;
+		transform: scale(0.98);
+		box-shadow: 0 5px 20px rgba(0, 0, 0, 0.3);
+		background: linear-gradient(45deg, #d33737, #ff5555);
 		color: white;
 	}
 
