a6447b0817b1670ffbe3f89910130cfc1ce92a74
hiredtocode <jasonhan@kakao.com>
2022-11-17 00:42:25 +0900

    Removed space between

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	display: flex;
 	max-width: 1200px;
-	justify-content: space-between;
 	align-items: flex-start;
 	padding: 130px 30px;
 	transition: 0.6s;
