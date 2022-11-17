6bbd38e1184c4e0b93abe8835756e153b542ad5d
hiredtocode <jasonhan@kakao.com>
2022-11-17 10:23:56 +0900

    Minor

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import TagSection from './styles/TagSection.styled.js';
+import { TagSection } from './styles/TagSection.styled.js';
 
 const Tags = ({ tags }) => {
 	return (
 		<TagSection>
-			<div>
-				{tags?.map((tag, index) => (
-					<p className='tag' key={index}>
-						{tag}
-					</p>
-				))}
-			</div>
+			{tags?.map((tag, index) => (
+				<p className='tag' key={index}>
+					{tag}
+				</p>
+			))}
 		</TagSection>
 	);
 };
