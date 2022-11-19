add7ac73cc33fd15485414d8e428d346bcb761c8
hiredtocode <jasonhan@kakao.com>
2022-11-19 21:23:28 +0900

    Line break styled

---
+++ src/components/styles/LineBreak.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const LineBreak = styled.div`
+	width: 100%;
+	height: 1px;
+	visibility: visible;
+	border-bottom: 1px solid ${({ theme }) => theme.colors.accent};
+`;
