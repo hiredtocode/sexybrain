adaf5d1e878140a6821179a903bb9bde768fc00c
hiredtocode <jasonhan@kakao.com>
2022-11-21 15:14:36 +0900

    Markdown preview added to create/update page

---
+++ src/components/styles/MarkdownContainer.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro';
+
+export const MarkdownContainer = styled.div`
+	width: 70%;
+	padding: 20px;
+	background: ${({ theme }) => theme.colors.card};
+	border-radius: 1rem;
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+	margin-left: 1em;
+	height: 80vh;
+`;
