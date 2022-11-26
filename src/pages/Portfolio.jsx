a0ebc01718764b7d6f4433191dd6cbffa7c772af
hiredtocode <jasonhan@kakao.com>
2022-11-26 23:17:09 +0900

    Project card description

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Flex from '../components/styles/Flex.styled.js';
-import { H1 } from '../components/styles/Title.styled';
+import { H1, H3 } from '../components/styles/Title.styled';
 import { Button } from '../components/styles/Button.styled.js';
 import {
 	ProjectContainer,
 	ProjectCard,
 } from '../components/styles/portfolio/ProjectContainer.styled.js';
+import SamplePic from '../assets/img/default.jpg';
+import { Description } from '../components/styles/Card.styled.js';
 
 const Portfolio = () => {
 	return (
@@ -22,7 +24,16 @@ const Portfolio = () => {
 					</Flex>
 				</nav>
 				<ProjectContainer>
-					<ProjectCard>Current Website</ProjectCard>
+					<ProjectCard>
+						<H3>Current Website</H3>
+						<img src={SamplePic} alt='Current Website' />
+						<Description>
+							Amet cupidatat pariatur proident sunt cillum. Lorem aliquip et labore velit veniam
+							sunt cillum dolore. Eu anim officia exercitation dolore. Amet dolor ipsum aliquip
+							ullamco dolore laborum elit deserunt laborum excepteur aute consectetur ea anim. Nulla
+							ea non sunt nisi sint aute cupidatat fugiat exercitation.
+						</Description>
+					</ProjectCard>
 					<ProjectCard>API - Angular</ProjectCard>
 					<ProjectCard>Nama Sushi</ProjectCard>
 					<ProjectCard>VS Code Theme</ProjectCard>
