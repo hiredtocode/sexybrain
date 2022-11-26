efb70c1f9f955057bc2f4582ff7235aab5eb5b4f
hiredtocode <jasonhan@kakao.com>
2022-11-26 22:50:24 +0900

    Basic contents

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import Flex from '../components/styles/Flex.styled.js';
+import { H1 } from '../components/styles/Title.styled';
 
 const Portfolio = () => {
 	return (
 		<main>
-			<Flex>
-				<p>Portfolio page.</p>
+			<Flex direction='column'>
+				<H1>Projects.</H1>
+				<nav>
+					<Flex direction='row'>
+						<div>All</div>
+						<div>React</div>
+						<div>Angular</div>
+						<div>Wordpress</div>
+						<div>Open Source</div>
+					</Flex>
+				</nav>
+				<section>
+					<ul>
+						<li>Current Website</li>
+						<li>API - Angular</li>
+						<li>Nama Sushi</li>
+						<li>VS Code Theme</li>
+					</ul>
+				</section>
 			</Flex>
 		</main>
 	);
