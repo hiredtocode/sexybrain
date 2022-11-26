5272b5bcae1b0d1b021195560369a6cb7aeb7dce
hiredtocode <jasonhan@kakao.com>
2022-11-26 23:03:12 +0900

    ProjectContainer for section

---
+++ src/components/styles/portfolio/ProjectContainer.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/macro';
+
+export const ProjectContainer = styled.section`
+	display: flex;
+
+	/* xs: '575.98px' */
+	@media (max-width: ${({ theme }) => theme.xs}) {
+		width: 100%;
+	}
+	/* s: '767.98px'  */
+	@media (max-width: ${({ theme }) => theme.s}) {
+	}
+	/* md: '991.98px'  */
+	@media (max-width: ${({ theme }) => theme.md}) {
+	}
+	/* lg: '1199.98px'  */
+	@media (max-width: ${({ theme }) => theme.lg}) {
+	} ;
+`;
