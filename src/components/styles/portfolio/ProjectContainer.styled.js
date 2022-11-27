d4ab364173dd3359d441d8872d183f2e33383119
hiredtocode <jasonhan@kakao.com>
2022-11-27 17:44:19 +0900

    Title shows on hover and card shows 50% width

--- src/components/styles/portfolio/ProjectContainer.styled.js
+++ src/components/styles/portfolio/ProjectContainer.styled.js
@@ -2,8 +2,11 @@ import styled from 'styled-components/macro';
 
 export const ProjectContainer = styled.section`
 	display: flex;
-	margin-top: 30px;
-	flex: wrap;
+	align-items: center;
+	max-width: 1200px;
+	padding: 40px 0;
+	justify-content: space-around;
+	flex-wrap: wrap;
 
 	/* xs: '575.98px' */
 	@media (max-width: ${({ theme }) => theme.xs}) {
@@ -20,10 +23,33 @@ export const ProjectContainer = styled.section`
 	} ;
 `;
 
+export const CardContainer = styled.div`
+	position: relative;
+`;
+
 export const ProjectCard = styled.div`
-	width: 50%;
+	width: 550px;
 	height: 400px;
+	margin: 10px;
+	border-radius: 15px;
 	overflow: hidden;
-	padding: 15px;
 	background: ${({ theme }) => theme.colors.card};
+
+	h3 {
+		position: absolute;
+		opacity: 0;
+		left: 0;
+		top: 0;
+		width: 100%;
+	}
+
+	&:hover {
+		h3 {
+			opacity: 1;
+			transition: 0.5s;
+			color: ${({ theme }) => theme.colors.accent2};
+			text-align: center;
+			background: ${({ theme }) => theme.colors.card};
+		}
+	}
 `;
