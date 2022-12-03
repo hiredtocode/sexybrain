a09f06160cd36cee06fc8b2d5454723a427029ad
hiredtocode <jasonhan@kakao.com>
2022-12-03 15:43:16 +0900

    Added hash mark to tags

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -4,10 +4,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Tags = (props) => {
 	const { tags, handleTagClick } = props;
+
 	return (
 		<TagSection>
 			{tags?.map((tag) => (
 				<TagButton className='tag' key={uuidv4()} onClick={() => handleTagClick(tag)}>
+					<span>#</span>
 					{tag}
 				</TagButton>
 			))}
