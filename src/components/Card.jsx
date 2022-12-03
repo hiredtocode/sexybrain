2083c218a9b951defda60e35e9ff2b2ca0fa5af0
hiredtocode <jasonhan@kakao.com>
2022-12-03 15:42:02 +0900

    Styled components logo chnaged

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -24,7 +24,7 @@ import sass from '../assets/img/sass.svg';
 import bootstrap from '../assets/img/bootstrap.svg';
 import materialUi from '../assets/img/material-ui.svg';
 import wordpress from '../assets/img/wordpress.svg';
-import styledComponents from '../assets/img/styled-components.svg';
+import styledComponents from '../assets/img/styledComponentsLogo.svg';
 import firebase from '../assets/img/firebase-icon.svg';
 import tailwindcss from '../assets/img/tailwindcss.svg';
 import programmer from '../assets/img/programmer.svg';
