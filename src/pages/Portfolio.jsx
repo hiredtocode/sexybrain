68e53f106e252e24a8c5a471f8ac881a32385260
hiredtocode <jasonhan@kakao.com>
2022-12-06 23:20:15 +0900

    Sqaure with arrow icon added to cards

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -20,7 +20,7 @@ import question from '../assets/img/question.svg';
 import { H3 } from '../components/styles/Title.styled';
 import Projects from '../projects/projects.json';
 import GithubButtonForPortfolio from '../components/GithubButtonForPortfolio';
-
+import SquareArrowIcon from '../assets/img/square-arrow-primary.svg';
 import {
 	ProjectCard,
 	CardContainer,
@@ -44,15 +44,20 @@ const Portfolio = () => {
 									<Flex>
 										<img className='image' src={project.image} alt={project.title} />
 										<Flex direction='column' grow='1'>
-											<Flex direction='column' style={{ Width: '100%' }}>
+											<Flex direction='row' style={{ Width: '100%' }} align='center'>
 												<H3>
-													{project.title} |{' '}
-													<GithubButtonForPortfolio
-														link={'https://github.com/hiredtocode/sexybrain'}
-													/>
+													{project.title}
+													<GithubButtonForPortfolio link={project.githubRepo} />
+													<a href={project.hyperlink} aria-label='{project.title}'>
+														<img
+															src={SquareArrowIcon}
+															alt='{project.title}'
+															style={{ maxWidth: '35px' }}
+														/>
+													</a>
 												</H3>
-												<span className='subText'> {project.date} </span>
 											</Flex>
+											<span className='subText'> {project.date} </span>
 
 											<ProjectDescription>
 												<p>{project.description}</p>
