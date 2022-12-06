e0d09248e4054214a5b097ec172b89817fd39a01
hiredtocode <jasonhan@kakao.com>
2022-12-06 23:16:49 +0900

    Unused import deleted

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { excerpt } from '../utility/excerpt';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
