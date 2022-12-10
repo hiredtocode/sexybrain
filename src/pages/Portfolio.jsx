9376efc1bce12125e92495afebdad454cbf50fa9
hiredtocode <jasonhan@kakao.com>
2022-12-10 17:08:43 +0900

    Removed unneccesary flex box

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -91,152 +91,150 @@ const Portfolio = () => {
 					projects.map((project) => (
 						<CardContainer key={uuidv4()}>
 							<ProjectCard>
-								<Flex>
-									<img className='image' src={project.image} alt={project.title} />
-									<Flex direction='column' grow='1'>
-										<Flex direction='row' style={{ Width: '100%' }} align='center' justfiy='center'>
-											<div>
-												<H3>{project.title}</H3>
-											</div>
-											{project.githubRepo ? (
-												<GithubButtonForPortfolio link={project.githubRepo} />
-											) : null}
-											{project.hyperlink ? (
-												<LinkButton link={project.hyperlink} title={project.title} />
-											) : null}
-										</Flex>
-										<span className='subText'> {project.date} </span>
-
-										<ProjectDescription>
-											<p>{project.description}</p>
-										</ProjectDescription>
+								<img className='image' src={project.image} alt={project.title} />
+								<Flex direction='column' grow='1'>
+									<Flex direction='row' style={{ Width: '100%' }} align='center' justfiy='center'>
+										<div>
+											<H3>{project.title}</H3>
+										</div>
+										{project.githubRepo ? (
+											<GithubButtonForPortfolio link={project.githubRepo} />
+										) : null}
+										{project.hyperlink ? (
+											<LinkButton link={project.hyperlink} title={project.title} />
+										) : null}
 									</Flex>
-									<Stack>
-										{project.stack.map((stack) => {
-											switch (stack) {
-												case 'JavaScript':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={javascript} alt={stack} />
-														</StackButton>
-													);
-												case 'TypeScript':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={typescript} alt={stack} />
-														</StackButton>
-													);
-												case 'React':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={react} alt={stack} />
-														</StackButton>
-													);
-												case 'Angular':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={angular} alt={stack} />
-														</StackButton>
-													);
-												case 'CSS':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={css} alt={stack} />
-														</StackButton>
-													);
-												case 'HTML':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={html} alt={stack} />
-														</StackButton>
-													);
-												case 'SASS':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={sass} alt={stack} />
-														</StackButton>
-													);
-												case 'Bootstrap':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={bootstrap} alt={stack} />
-														</StackButton>
-													);
-												case 'Material UI':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={materialUi} alt={stack} />
-														</StackButton>
-													);
-												case 'Tailwind CSS':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={tailwindcss} alt={stack} />
-														</StackButton>
-													);
-												case 'Styled Components':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={styledComponents} alt={stack} />
-														</StackButton>
-													);
-												case 'What I learned':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={programmer} alt={stack} />
-														</StackButton>
-													);
-												case 'Firebase':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={firebase} alt={stack} />
-														</StackButton>
-													);
-												case 'Wordpress':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={wordpress} alt={stack} />
-														</StackButton>
-													);
-												case 'PHP':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={php} alt={stack} />
-														</StackButton>
-													);
-												case 'VSCode':
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={visualStudioCode} alt={stack} />
-														</StackButton>
-													);
-												default:
-													return (
-														<StackButton key={uuidv4()}>
-															<span>{stack}</span>
-															<img src={question} alt={stack} />
-														</StackButton>
-													);
-											}
-										})}
-									</Stack>
+									<span className='subText'> {project.date} </span>
+
+									<ProjectDescription>
+										<p>{project.description}</p>
+									</ProjectDescription>
 								</Flex>
+								<Stack>
+									{project.stack.map((stack) => {
+										switch (stack) {
+											case 'JavaScript':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={javascript} alt={stack} />
+													</StackButton>
+												);
+											case 'TypeScript':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={typescript} alt={stack} />
+													</StackButton>
+												);
+											case 'React':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={react} alt={stack} />
+													</StackButton>
+												);
+											case 'Angular':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={angular} alt={stack} />
+													</StackButton>
+												);
+											case 'CSS':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={css} alt={stack} />
+													</StackButton>
+												);
+											case 'HTML':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={html} alt={stack} />
+													</StackButton>
+												);
+											case 'SASS':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={sass} alt={stack} />
+													</StackButton>
+												);
+											case 'Bootstrap':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={bootstrap} alt={stack} />
+													</StackButton>
+												);
+											case 'Material UI':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={materialUi} alt={stack} />
+													</StackButton>
+												);
+											case 'Tailwind CSS':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={tailwindcss} alt={stack} />
+													</StackButton>
+												);
+											case 'Styled Components':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={styledComponents} alt={stack} />
+													</StackButton>
+												);
+											case 'What I learned':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={programmer} alt={stack} />
+													</StackButton>
+												);
+											case 'Firebase':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={firebase} alt={stack} />
+													</StackButton>
+												);
+											case 'Wordpress':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={wordpress} alt={stack} />
+													</StackButton>
+												);
+											case 'PHP':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={php} alt={stack} />
+													</StackButton>
+												);
+											case 'VSCode':
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={visualStudioCode} alt={stack} />
+													</StackButton>
+												);
+											default:
+												return (
+													<StackButton key={uuidv4()}>
+														<span>{stack}</span>
+														<img src={question} alt={stack} />
+													</StackButton>
+												);
+										}
+									})}
+								</Stack>
 							</ProjectCard>
 						</CardContainer>
 					))}
