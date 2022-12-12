9036972d2ff2b6ac4dfcf9f7985867441ec2c208
hiredtocode <jasonhan@kakao.com>
2022-12-13 00:51:20 +0900

    Footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { FooterContainer } from './styles/FooterContainer.sytled';
+
+function Footer() {
+	return (
+		<FooterContainer>
+			<a href='https://github.com/hiredtocode/sexybrain'>Designed & Built by Jason Han</a>
+		</FooterContainer>
+	);
+}
+
+export default Footer;
