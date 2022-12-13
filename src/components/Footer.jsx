c5bcb9ff63a0e8070d69921ebbf9e7e20a063475
hiredtocode <jasonhan@kakao.com>
2022-12-14 00:55:46 +0900

    Text glitch fix?

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import { FooterContainer } from './styles/FooterContainer.sytled';
 function Footer() {
 	return (
 		<FooterContainer>
-			<a href='https://github.com/hiredtocode/sexybrain'>Designed & Built by Jason Han</a>
+			<a href='https://github.com/hiredtocode/sexybrain'>
+				<span>Designed & Built by Jason Han</span>
+			</a>
 		</FooterContainer>
 	);
 }
