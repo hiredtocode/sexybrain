b0b9454cf2b8a3b90227714eb0b3223097b5abc4
hiredtocode <jasonhan@kakao.com>
2022-12-14 17:25:05 +0900

    Reduced padding on narrower width view

--- src/components/DarkLightMode.jsx
+++ src/components/DarkLightMode.jsx
@@ -17,6 +17,21 @@ const DarkLightMode = () => {
 		justify-content: center;
 		border-radius: 50%;
 
+		/* xs: '575.98px' */
+		@media (max-width: ${({ theme }) => theme.xs}) {
+		}
+		/* s: '767.98px'  */
+		@media (max-width: ${({ theme }) => theme.s}) {
+			margin-right: 5px;
+		}
+		/* md: '991.98px'  */
+		@media (max-width: ${({ theme }) => theme.md}) {
+			padding: 1rem;
+		}
+		/* lg: '1199.98px'  */
+		@media (max-width: ${({ theme }) => theme.lg}) {
+		}
+
 		svg {
 			fill: ${({ theme }) => theme.colors.primary};
 		}
