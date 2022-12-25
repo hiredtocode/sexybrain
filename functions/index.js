8751a4b26b864b8a4e2aeb35891ef45bed98c8bc
hiredtocode <jasonhan@kakao.com>
2022-12-26 01:09:11 +0900

    ESLint

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,19 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-admin.initializeApp();
+admin.initializeApp()
 
 exports.addAdminRole = functions.https.onCall(async data => {
 	// Get user and add custom claim (admin)
 	try {
-		const user = await admin.auth().getUserByEmail(data.email);
+		const user = await admin.auth().getUserByEmail(data.email)
 		await admin.auth().setCustomUserClaims(user.uid, {
 			admin: true,
-		});
+		})
 		return {
 			message: `Success! ${data.email} has been made an admin`,
-		};
+		}
 	} catch (error) {
-		return error;
+		return error
 	}
-});
+})
