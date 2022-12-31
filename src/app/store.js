4fdeb7d317477271f8bc7aacef8df36bc36b071f
hiredtocode <jasonhan@kakao.com>
2023-01-01 07:52:40 +0900

    Production env

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,26 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import resetButtonReducer from '../features/buttonState/resetButtonReducer';
-import counterReducer from '../features/counter/counterSlice';
-import darkmodeReducer from '../features/darkMode/darkmodeReducer';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import resetButtonReducer from '../features/buttonState/resetButtonReducer'
+import counterReducer from '../features/counter/counterSlice'
+import darkmodeReducer from '../features/darkMode/darkmodeReducer'
 
 const rootReducer = combineReducers({
 	resetButton: resetButtonReducer,
 	darkmode: darkmodeReducer,
 	counter: counterReducer,
-});
+})
 
-const store = configureStore({ reducer: rootReducer });
+let store
 
-export default store;
+if (
+	process.env.NODE_ENV === 'production' ||
+	!window.__REDUX_DEVTOOLS_EXTENSION__
+) {
+	store = configureStore({ reducer: rootReducer })
+} else {
+	store = configureStore(
+		{ reducer: rootReducer },
+		window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+}
+
+export default store
