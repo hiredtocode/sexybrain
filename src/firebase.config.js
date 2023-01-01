bc01598b97fd43b32cd3cbb2eec8789f5f8c70e0
hiredtocode <jasonhan@kakao.com>
2023-01-01 10:17:15 +0900

    eslint

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,10 +1,10 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 // import { getAnalytics } from "firebase/analytics";
-import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getFunctions } from 'firebase/functions';
-import { getStorage } from 'firebase/storage';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getFunctions } from 'firebase/functions'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -15,18 +15,18 @@ const firebaseConfig = {
 	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
 	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 const appCheck = initializeAppCheck(app, {
 	provider: new ReCaptchaV3Provider(process.env.REACT_APP_CAPTCHA_APPCHECK),
 	isTokenAutoRefreshEnabled: true,
-});
-const auth = getAuth(app);
-const storage = getStorage(app);
-const db = getFirestore(app);
-const functions = getFunctions(app);
+})
+const auth = getAuth(app)
+const storage = getStorage(app)
+const db = getFirestore(app)
+const functions = getFunctions(app)
 
 // const analytics = getAnalytics(app);
-export { auth, db, storage, functions };
+export { auth, db, storage, functions }
